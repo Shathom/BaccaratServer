883622678e03e2da6eb45f4db24ca95a3dc0c3e2
Sharon Thomeh <sharon.thomeh@gmail.com>
2021-10-30 23:40:20 -0500

    added some necessary classes

---
+++ src/main/java/BaccaratDealer.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class BaccaratDealer {
+	
+	public ArrayList<Card> deck;
+	
+	
+	// generateDeck will generate a new standard 52 card deck where each card is an instance of the Card class in the ArrayList<Card> deck.
+	public void generateDeck() {
+		
+	}
+	// dealHand will deal two cards and return them in an ArrayList<Card>
+	public ArrayList<Card> dealHand() {
+		
+	}
+	// drawOne will deal a single card and return it.
+	public Card drawOne() {
+		
+	}
+	// shuffleDeck will create a new deck of 52 cards and “shuffle”; randomize the cards in that ArrayList<Card>. 
+	public void shuffleDeck() {
+		
+	}
+	// deckSize will just return how many cards are in this.deck at any given time.
+	public int deckSize() {
+		
+	}
+	
+}
---
+++ src/main/java/BaccaratInfo.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+public class BaccaratInfo implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+}
