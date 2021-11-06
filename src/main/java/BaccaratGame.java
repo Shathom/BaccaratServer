a547fb8df34174d2832c81836424e2df37deceb3
Sharon Thomeh <sharon.thomeh@gmail.com>
2021-11-05 21:16:28 -0500

    Update BaccaratGame.java
    added the function to evaluate winnings and determine if there was win or a loss

--- src/main/java/BaccaratGame.java
+++ src/main/java/BaccaratGame.java
@@ -4,13 +4,62 @@ public class BaccaratGame {
 	
 	public ArrayList<Card> playerHand;
 	public ArrayList<Card> bankerHand;
+	public String whoToBetOn;
 	public BaccaratDealer theDealer;
 	public double currentBet;
 	public double totalWinnings;
 	
+	
+	BaccaratGame(double currentBet, String whoToBetOn) {
+		this.currentBet = currentBet;
+		this.whoToBetOn = whoToBetOn;
+		theDealer.generateDeck();
+		playerHand = theDealer.dealHand();
+		bankerHand = theDealer.dealHand();
+	}
+	
+	public boolean winOrLoss(ArrayList<Card> playerHand, ArrayList<Card> bankerHand) {
+		Card drawCard = theDealer.drawOne();
+		Card noDraw = null;
+		if (BaccaratGameLogic.handTotal(bankerHand) == 9 ||
+			BaccaratGameLogic.handTotal(bankerHand) == 8 ||
+			BaccaratGameLogic.handTotal(playerHand) == 9 ||
+			BaccaratGameLogic.handTotal(playerHand) == 8 ) {
+			
+			return BaccaratGameLogic.whoWon(playerHand, bankerHand) == whoToBetOn;
+				
+		} else { 
+			if (BaccaratGameLogic.evaluatePlayerDraw(playerHand)) {
+				playerHand.add(drawCard);
+				if (BaccaratGameLogic.evaluateBankerDraw(bankerHand, drawCard)) {
+					bankerHand.add(theDealer.drawOne());
+					return BaccaratGameLogic.whoWon(playerHand, bankerHand) == whoToBetOn;
+				} else {
+					return BaccaratGameLogic.whoWon(playerHand, bankerHand) == whoToBetOn;
+				}
+			} else {
+				if (BaccaratGameLogic.evaluateBankerDraw(bankerHand, noDraw)) {
+					bankerHand.add(theDealer.drawOne());
+					return BaccaratGameLogic.whoWon(playerHand, bankerHand) == whoToBetOn;
+				} else {
+					return BaccaratGameLogic.whoWon(playerHand, bankerHand) == whoToBetOn;
+				}
+			}
+		}	
+	}
+	
+	
 	// This method will determine if the user won or lost their bet and return the amount won or lost based on the value in currentBet.
 	public double evaluateWinnings() {
-		double d = 0;
-		return d;
+		if (winOrLoss(playerHand, bankerHand)) {
+			if (BaccaratGameLogic.whoWon(playerHand, bankerHand) == "Player" ||
+			    BaccaratGameLogic.whoWon(playerHand, bankerHand) == "Banker") {
+				totalWinnings = currentBet * 2;
+			} else {
+				totalWinnings = (currentBet * 8) + currentBet;
+			}
+		}
+		return totalWinnings;
 	}
+	
 }
