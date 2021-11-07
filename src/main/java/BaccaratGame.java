6a03915bf214be4939c58b0b1c0fbe22a0f8f4fe
Sharon Thomeh <sharon.thomeh@gmail.com>
2021-11-06 19:05:19 -0500

    Update BaccaratGame.java
    modified the logic of the game for correction

--- src/main/java/BaccaratGame.java
+++ src/main/java/BaccaratGame.java
@@ -2,64 +2,63 @@ import java.util.ArrayList;
 
 public class BaccaratGame {
 	
-	public ArrayList<Card> playerHand;
-	public ArrayList<Card> bankerHand;
-	public String whoToBetOn;
-	public BaccaratDealer theDealer;
-	public double currentBet;
-	public double totalWinnings;
+	private ArrayList<Card> playerHand;
+	private ArrayList<Card> bankerHand;
+	private String bettingType;
+	private BaccaratDealer theDealer;
+	private double currentBet;
+	private double totalWinnings;
 	
 	
 	BaccaratGame(double currentBet, String whoToBetOn) {
 		this.currentBet = currentBet;
-		this.whoToBetOn = whoToBetOn;
+		this.bettingType = whoToBetOn;
 		theDealer.generateDeck();
 		playerHand = theDealer.dealHand();
 		bankerHand = theDealer.dealHand();
 	}
 	
-	public boolean winOrLoss(ArrayList<Card> playerHand, ArrayList<Card> bankerHand) {
+	
+	public void drawCards(ArrayList<Card> playerHand, ArrayList<Card> bankerHand) {
 		Card drawCard = theDealer.drawOne();
 		Card noDraw = null;
 		if (BaccaratGameLogic.handTotal(bankerHand) == 9 ||
 			BaccaratGameLogic.handTotal(bankerHand) == 8 ||
 			BaccaratGameLogic.handTotal(playerHand) == 9 ||
 			BaccaratGameLogic.handTotal(playerHand) == 8 ) {
-			
-			return BaccaratGameLogic.whoWon(playerHand, bankerHand) == whoToBetOn;
-				
-		} else { 
+			return;
+		} else {
 			if (BaccaratGameLogic.evaluatePlayerDraw(playerHand)) {
 				playerHand.add(drawCard);
 				if (BaccaratGameLogic.evaluateBankerDraw(bankerHand, drawCard)) {
 					bankerHand.add(theDealer.drawOne());
-					return BaccaratGameLogic.whoWon(playerHand, bankerHand) == whoToBetOn;
+					return;
 				} else {
-					return BaccaratGameLogic.whoWon(playerHand, bankerHand) == whoToBetOn;
+					return;
 				}
 			} else {
 				if (BaccaratGameLogic.evaluateBankerDraw(bankerHand, noDraw)) {
 					bankerHand.add(theDealer.drawOne());
-					return BaccaratGameLogic.whoWon(playerHand, bankerHand) == whoToBetOn;
+					return;
 				} else {
-					return BaccaratGameLogic.whoWon(playerHand, bankerHand) == whoToBetOn;
+					return;
 				}
 			}
-		}	
-	}
-	
+	    }	
+   }
 	
 	// This method will determine if the user won or lost their bet and return the amount won or lost based on the value in currentBet.
 	public double evaluateWinnings() {
-		if (winOrLoss(playerHand, bankerHand)) {
-			if (BaccaratGameLogic.whoWon(playerHand, bankerHand) == "Player" ||
-			    BaccaratGameLogic.whoWon(playerHand, bankerHand) == "Banker") {
-				totalWinnings = currentBet * 2;
+		drawCards(playerHand, bankerHand);
+			if (BaccaratGameLogic.whoWon(playerHand, bankerHand) == bettingType) {
+				if (bettingType == "Player" || bettingType == "Banker") {
+					totalWinnings = currentBet * 2;
+				} else {
+					totalWinnings = (currentBet * 8) + currentBet;
+				}
 			} else {
-				totalWinnings = (currentBet * 8) + currentBet;
+				totalWinnings = -currentBet;
 			}
-		}
 		return totalWinnings;
 	}
-	
 }
