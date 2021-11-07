52118a53de3c89e24201bf663ffee3ff19588b5f
Sharon Thomeh <sharon.thomeh@gmail.com>
2021-11-06 19:06:26 -0500

    Update BaccaratInfo.java
    added new fields to the serializable class

--- src/main/java/BaccaratInfo.java
+++ src/main/java/BaccaratInfo.java
@@ -1,11 +1,25 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class BaccaratInfo implements Serializable {
-
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
+	public String bettingType;
+	public double bettingAmount;
+	public boolean quit;
+	public ArrayList<String> playerHand;
+	public ArrayList<String> bankerHand;
+	public boolean naturalWin;
+	public boolean playerDraw;
+	public boolean bankerDraw;
+	public double totalWinnings;
+	public double currentWinnings;
+	public String gameResult;
+	
+	BaccaratInfo() {
+		
+    }
 	
 
 }
+
